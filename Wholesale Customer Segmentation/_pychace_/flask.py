67aa17d838a0c436a92ff28aba398537105fa992
CharanSathvik <144941369+CharanSathvik@users.noreply.github.com>
2024-01-03 11:31:59 +0530

    Add files via upload

---
+++ Wholesale Customer Segmentation/_pychace_/flask.py
@@ -0,0 +1,7 @@
+from flask import Flask,render_template,url_for,request
+import pickle as p
+import pickle
+from flask import Flask,request,jsonify,render_template
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
